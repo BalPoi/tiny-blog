1853c6d40ebaa3a08dbad6658db73cac6651d718
Pavel Bal <pbal00@mail.ru>
2022-06-25 16:38:35 +0300

    feat: improve routing
    Auto redirect to #feed bypassing #login if user is remembered.

--- src/js/hash-router.js
+++ src/js/hash-router.js
@@ -1,55 +1,47 @@
 const pageTitle = "Tiny Blog";
 // create an object that maps the url to the template, title, and description
 const routes = {
-	404: {
-		template: "/pages/404.html",
-		style: "",
-		title: "404 | " + pageTitle,
-		description: "Page not found",
-	},
-	"/": {
-		template: "/pages/index.html",
-		style: "/pages/index.css",
-		title: pageTitle,
-		description: "This is the home page",
-	},
-	login: {
-		template: "/pages/login.html",
-		style: "/pages/login.css",
-		title: "Log in | " + pageTitle,
-		description: "This is the log in page",
-	},
-	feed: {
-		template: "/pages/feed.html",
-		style: "/pages/feed.css",
-		title: "Feed | " + pageTitle,
-		description: "This is the feed page",
-	},
+  404: {
+    template: "/pages/404.html",
+    style: "",
+    title: "404 | " + pageTitle,
+  },
+  "/": {
+    template: "/pages/index.html",
+    style: "/pages/index.css",
+    title: pageTitle,
+  },
+  login: {
+    template: "/pages/login.html",
+    style: "/pages/login.css",
+    title: "Log in | " + pageTitle,
+  },
+  feed: {
+    template: "/pages/feed.html",
+    style: "/pages/feed.css",
+    title: "Feed | " + pageTitle,
+  },
 };
 
 // create a function that watches the url and calls the urlLocationHandler
 const locationHandler = async () => {
-	// get the url path, replace hash with empty string
-	var location = window.location.hash.replace("#", "");
-	// if the path length is 0, set it to primary page route
-	if (location.length == 0) {
-		location = "/";
-	}
-	// get the route object from the routes object
-	const route = routes[location] || routes["404"];
-	// link required css styles
-	const cssLink = document.getElementById('pageStyle');
-	cssLink.href = route.style;
-	// get the html from the template
-	const html = await fetch(route.template).then((response) => response.text());
-	// set the content of the content div to the html
-	document.getElementById("root").innerHTML = html;
-	// set the title of the document to the title of the route
-	document.title = route.title;
-	// set the description of the document to the description of the route
-	// document
-	// 	.querySelector('meta[name="description"]')
-	// 	.setAttribute("content", route.description);
+  // get the url path, replace hash with empty string
+  var location = window.location.hash.replace("#", "");
+  // if the path length is 0, set it to primary page route
+  if (location.length == 0) {
+    location = "/";
+  }
+  // get the route object from the routes object
+  const route = routes[location] || routes["404"];
+  // link required css styles
+  const cssLink = document.getElementById('pageStyle');
+  cssLink.href = route.style;
+  // get the html from the template
+  const html = await fetch(route.template).then((response) => response.text());
+  // set the content of the content div to the html
+  document.getElementById("root").innerHTML = html;
+  // set the title of the document to the title of the route
+  document.title = route.title;
 };
 // create a function that watches the hash and calls the urlLocationHandler
 window.addEventListener("hashchange", locationHandler);
--- src/js/localStorageInitialization.js
+++ src/js/localStorageInitialization.js
@@ -1,11 +1,16 @@
-const localStorageInit = () => {
+const initUsers = () => {
   window.localStorage.setItem('users', JSON.stringify(users));
-  console.log(users);
+  console.log('Users: ', users);
 }
-
+const initCurrUser = () => {
+  window.localStorage.setItem('currUser', JSON.stringify(currUser));
+  console.log('Current: ', currUser);
+}
 let users = [
-  {login: 'pavel', passwod: '1234'},
-  {login: 'igar', passwod: 'igar'},
-  {login: 'admin', passwod: 'adminpassword'},
-  {login: 'user', passwod: 'userpassword'},
+  {login: 'pavel', password: '1234'},
+  {login: 'igar', password: 'igar'},
+  {login: 'admin', password: 'adminpassword'},
+  {login: 'user', password: 'userpassword'},
 ];
+
+let currUser = {login: 'pavel', password: '1234'};
